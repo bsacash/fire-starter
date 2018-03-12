c91017da38290b4954cc509b71819ba3a4aa8730
Brian S <bsacash@gmail.com>
2018-03-11 22:32:41 -0400

    add folder links

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from helper import query, read_config
+from helper import query, read_config, get_contents
 
 from helper import Bookmark, Folder
 
@@ -13,38 +13,19 @@ def index():
     q = "SELECT id from moz_bookmarks where title == 'Bookmarks Toolbar';"
     master_id = query(q,db,mode="one")[0]
 
-    # Fetch all folder items in the Bookmarks Toolbar folder
-    q = "SELECT id, type, parent, title from moz_bookmarks where parent == {0} and type==2;".format(master_id)
-    folders = query(q,db,mode="all")
-    folders = [Folder(folder_id = f[0], title = f[3]) for f in folders]
-
-    #BOOKMARKS
-    # Fetch all bookmark items in the Bookmarks Toolbar folder
-    q = "SELECT id, type, fk, parent, title from moz_bookmarks where parent=={0} and type==1;".format(master_id)
-    bookmarks = query(q,db,mode="all")
-    bookmarks = [Bookmark(url_id = b[2], title = b[4]) for b in bookmarks]
-
-    # Fetch bookmark urls (batch)
-    url_ids = tuple([bm.id for bm in bookmarks])
-    q = "SELECT id, url from moz_places WHERE id in {0}".format(url_ids)
-    r = query(q, db, mode="all")
-    r = {entry[0]:entry[1] for entry in r}
-
-    #Join urls with Bookmarks
-    for bm in bookmarks:
-        temp_id = bm.id
-        try:
-            bm.url = r[bm.id]
-        except:
-            pass
+    folders, bookmarks = get_contents(master_id, db)
 
     return render_template('index.html', bookmarks=bookmarks, folders=folders)
 
 @app.route('/folder', methods=['POST'])
 def folder():
-    folder_id = request.form['folder_click'])
+    folder_id = request.form['folder_click']
 
-    return (request.form['folder_click'])
+    db = read_config("config.config")
+
+    folders, bookmarks = get_contents(folder_id,db)
+
+    return render_template('index.html', bookmarks=bookmarks, folders=folders)
 
 if __name__ == "__main__":
     app.run()
