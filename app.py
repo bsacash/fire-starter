258a7703194157efddfadd0d31292ccd43853b23
Brian S <bsacash@gmail.com>
2018-03-14 22:49:20 -0400

    add search redirect

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from helper import query, read_config, get_contents
 
 from helper import Bookmark, Folder
@@ -8,24 +8,24 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     db = read_config("config.config")
-
     # Get Bookmarks Toolbar ID
     q = "SELECT id from moz_bookmarks where title == 'Bookmarks Toolbar';"
     master_id = query(q,db,mode="one")[0]
-
     folders, bookmarks = get_contents(master_id, db)
-
     return render_template('index.html', bookmarks=bookmarks, folders=folders)
 
 @app.route('/folder', methods=['POST'])
 def folder():
     folder_id = request.form['folder_click']
-
     db = read_config("config.config")
-
     folders, bookmarks = get_contents(folder_id,db)
-
     return render_template('index.html', bookmarks=bookmarks, folders=folders)
+
+@app.route('/search', methods=['POST'])
+def search():
+    search_term = request.form['search_input']
+    suffix = "?q={0}".format(search_term)
+    return redirect("http://www.duckduckgo.com/" + suffix)
 
 if __name__ == "__main__":
     app.run()
